d720b22adea49daa16cf9168944708ba7f9ebc78
Dragonisser <dragonisser@googlemail.com>
2023-01-15 01:44:17 +0100

    fix: replace jquery and fix plugin

--- TwitchTheatherButton.js
+++ TwitchTheatherButton.js
@@ -1,26 +1,35 @@
 // ==UserScript==
-// @name         Twitch Theather Button
+// @name         Twitch Theatre Button
 // @version      1.0
 // @description  Adds a button to quickly get to Twitch Theater
 // @author       Dragonisser
 // @include      https://www.youtube.com/*
 // @grant        GM_addStyle
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // ==/UserScript==
 
-GM_addStyle('.theather-button { background-color: transparent; color: white;  cursor: pointer; border: none;}');
+GM_addStyle('.theather-button { color: var(--yt-spec-text-primary); background-color: var(--yt-spec-badge-chip-background); cursor: pointer; padding: 0 16px; height: 36px; font-size: 14px; line-height: 36px; border-radius: 18px; border: none; margin-right: 8px;}');
 
 (function() {
     'use strict';
 
     function prependButton() {
         let videoUrl = window.location.href.split("=")[1];
-        let theaterUrl = "'https://twitchtheater.tv/v=" + videoUrl + "'";
-        let nav = jQuery('div#primary-inner').find('div#info-contents').find('div#menu').find('div#top-level-buttons-computed');
-        let button = '<button class="theather-button" onclick="window.open(' + theaterUrl +')">Twitch Theather</button>';
+        let theaterUrl = "https://twitchtheater.tv/v=" + videoUrl;
+        let nav = document.querySelector('ytd-watch-metadata').querySelector('#menu').querySelector('div#top-level-buttons-computed');
+
+        let button = document.createElement('button');
+        button.setAttribute('id', 'button-twitch-theatre');
+        button.append('Twitch Theatre');
+        button.classList.add('theather-button');
+        button.addEventListener('click', function() {
+            window.open(theaterUrl);
+        });
 
         if (nav) {
-            nav.prepend(button);
+            let btn = document.querySelector('#button-twitch-theatre');
+            if (!btn) {
+                nav.prepend(button);
+            }
         }
     };
 
