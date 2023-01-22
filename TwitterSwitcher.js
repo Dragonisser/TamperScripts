c42c0807e706ca2742231d5598ee1bc1867e987e
Dragonisser <dragonisser@googlemail.com>
2023-01-22 01:27:10 +0100

    Update TwitterSwitcher.js

--- TwitterSwitcher.js
+++ TwitterSwitcher.js
@@ -7,8 +7,8 @@
 // @match        https://twitter.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
 // @grant        none
-// @updateURL    https://github.com/Dragonisser/TamperScripts/blob/master/TwitterSwitcher.js
-// @downloadURL  https://github.com/Dragonisser/TamperScripts/blob/master/TwitterSwitcher.js
+// @updateURL    https://raw.githubusercontent.com/Dragonisser/TamperScripts/master/TwitterSwitcher.js
+// @downloadURL  https://raw.githubusercontent.com/Dragonisser/TamperScripts/master/TwitterSwitcher.js
 // ==/UserScript==
 
 (function () {
